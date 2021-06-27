3713daf4c1836704f58174b2b9a21ad5ae6e138f
tkddnrdlz <tkddnrdlz@naver.com>
2021-06-28 07:42:06 +0900

    2a

---
+++ src/main/java/www/dream/com/common/attachFile/control/UploadController.java
@@ -0,0 +1,191 @@
+package www.dream.com.common.attachFile.control;
+
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URLEncoder;
+import java.nio.file.Files;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
+
+import javax.imageio.ImageIO;
+
+import org.jcodec.api.FrameGrab;
+import org.jcodec.common.model.Picture;
+import org.jcodec.scale.AWTUtil;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.FileCopyUtils;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import net.coobird.thumbnailator.Thumbnailator;
+import www.dream.com.common.attachFile.model.AttachFileVO;
+import www.dream.com.common.attachFile.model.MultimediaType;
+import www.dream.com.framework.util.FileUtil;
+import www.dream.com.framework.util.StringUtil;
+
+@Controller
+@RequestMapping("/uploadFiles/*")
+public class UploadController {
+	private static final String UPLOAD_FOLDER = "C:\\uploadedFiles";
+	@GetMapping("uploadByAjax")
+	public void uploadByAjax() {
+		
+	}
+	@PostMapping(value = "upload", produces=MediaType.APPLICATION_JSON_UTF8_VALUE)
+	@ResponseBody
+	public ResponseEntity<List<String>> uploadFilesByAjax(@RequestParam("uploadFile") MultipartFile[] uploadFiles) {
+		List<AttachFileVO> listAttachFileVO = new ArrayList<>();
+		File uploadPath = new File(UPLOAD_FOLDER, getFolderName());
+		if (! uploadPath.exists()) {
+			//필요한 폴더 구조가 없다면 그 전체를 만들어 준다.
+			uploadPath.mkdirs();
+		}
+		//업로드 할때 사용할 수 있는거임
+		for(MultipartFile uf : uploadFiles) {
+			AttachFileVO attachFileVO = new AttachFileVO();
+			attachFileVO.setSavedFolderPath(uploadPath.getAbsolutePath());
+			
+			String originalFilename = uf.getOriginalFilename();
+			String pureFilename = originalFilename.substring(originalFilename.lastIndexOf("\\") + 1);
+		
+			attachFileVO.setPureFileName(pureFilename);
+			//UUID : 여러 사용자가 올리는 파일의 이름이 같더라도 모두 수용할 수 있다.
+			String uuid = UUID.randomUUID().toString();
+			attachFileVO.setUuid(uuid);
+
+			String pureSaveFileName = attachFileVO.getPureSaveFileName();
+			
+			//uploadPath경로에 
+			File save = new File(uploadPath, pureSaveFileName);
+			try {
+				uf.transferTo(save);
+				makeThumnail(uploadPath, save, pureSaveFileName, attachFileVO);
+				attachFileVO.setMultimediaType(MultimediaType.identifyMultimediaType(save));
+			} catch (IllegalStateException | IOException e) {
+				e.printStackTrace();
+			}
+			//buildAuxInfo : 부가정보 생성하는 함수
+			listAttachFileVO.add(attachFileVO);
+		}
+		List<String> ret = listAttachFileVO.stream().map(vo -> vo.getJson()).collect(Collectors.toList());
+		
+		return new ResponseEntity<>(ret, HttpStatus.OK);
+	}
+	
+	@GetMapping("display")
+	@ResponseBody
+	public ResponseEntity<byte[]> getThumbnailFile(@RequestParam("fileName") String fileName) {
+		File file = new File(fileName);
+		ResponseEntity<byte[]> res = null;
+		HttpHeaders headers = new HttpHeaders();
+		try {
+			headers.add("Content-Type", Files.probeContentType(file.toPath()));
+			res = new ResponseEntity<byte[]>(FileCopyUtils.copyToByteArray(file), headers, HttpStatus.OK);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return res;
+	}
+	
+	/**
+	 * @param fileName : C:\\uploadedFiles\2021\06\22\myfile.txt  Full File Path 형식으로 관리 할 것임.
+	 * @return
+	 */
+	@GetMapping(value = "download", produces = MediaType.APPLICATION_OCTET_STREAM_VALUE)
+	@ResponseBody
+	public ResponseEntity<Resource> downloadFile(@RequestHeader("User-Agent") String userAgent, @RequestParam("fileName") String fileName) {
+		Resource resource = new FileSystemResource(fileName);
+		if (!resource.exists()) {
+			return new ResponseEntity(HttpStatus.NOT_FOUND);
+		}
+		String resourceFilename = AttachFileVO.fillterPureFileName(resource.getFilename());
+		HttpHeaders headers = new HttpHeaders();
+		try {
+			String downloadFileName = null;
+			if (userAgent.contains("Trident") || userAgent.contains("Edge")) {
+				downloadFileName = URLEncoder.encode(resourceFilename, "URF-8");
+			} else {
+				downloadFileName = new String(resourceFilename.getBytes("UTF-8"),"ISO-8859-1");
+			}
+			headers.add("Content-Disposition","attachment; filename=" + downloadFileName);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return new ResponseEntity<>(resource, headers, HttpStatus.OK);
+	}
+	
+	@PostMapping(value = "deleteFile")
+	@ResponseBody
+	public ResponseEntity<String> cancelAttach(AttachFileVO attachVO) {
+		try {
+			File pureSaveFileName = new File(attachVO.getSavedFolderPath(), attachVO.getPureSaveFileName());
+			pureSaveFileName.delete();
+			if (StringUtil.hasInfo(attachVO.getPureThumbnailFileName())) {
+				new File(attachVO.getSavedFolderPath(), attachVO.getPureThumbnailFileName()).delete();
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return new ResponseEntity<String>("삭제성공", HttpStatus.OK);
+	}
+	
+	//오늘 날짜의 폴더 이름을 줘라.
+	private String getFolderName() {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		return sdf.format(new Date()).replace('-', File.separatorChar);
+	}
+
+	private void makeThumnail(File uploadPath, File uploadedFile, String pureSaveFileName, AttachFileVO attachFileVO) {
+		MultimediaType multimediaType = MultimediaType.identifyMultimediaType(uploadedFile);
+		if (multimediaType == MultimediaType.image) {
+			String pureThumbnailFileName =  AttachFileVO.THUMBNAIL_FILE_PREFIX + pureSaveFileName;
+			attachFileVO.setPureThumbnailFileName(pureThumbnailFileName);
+			File thumbnailFile = new File(uploadPath, pureThumbnailFileName);
+			try {
+				Thumbnailator.createThumbnail(uploadedFile, thumbnailFile, 100, 100);
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		} else if (multimediaType == MultimediaType.video) {
+			pureSaveFileName = FileUtil.truncateExt(pureSaveFileName);
+			String pureThumbnailFileName =  AttachFileVO.THUMBNAIL_FILE_PREFIX + pureSaveFileName + ".png";
+			attachFileVO.setPureThumbnailFileName(pureThumbnailFileName);
+			File thumbnailFile = new File(uploadPath, pureThumbnailFileName);
+			try {
+				int frameNumber = 0;
+				//video 파일에서 첫번째 프레임의  이미지를 가져오기
+				Picture picture = FrameGrab.getFrameFromFile(uploadedFile, frameNumber);
+				BufferedImage bufferedImage = AWTUtil.toBufferedImage(picture);
+				ByteArrayOutputStream os = new ByteArrayOutputStream(); 
+				ImageIO.write(bufferedImage, "png", os);
+				InputStream is = new ByteArrayInputStream(os.toByteArray());
+				FileOutputStream fileOutputStream = new FileOutputStream(thumbnailFile);
+				//가져온 이미지를 Thumbnail로 만들기 
+				Thumbnailator.createThumbnail(is, fileOutputStream, 100, 100);
+				fileOutputStream.close();
+			} catch (Exception e) {
+				e.printStackTrace();
+			}
+		}
+	}
+}
---
+++ src/main/webapp/WEB-INF/views/common/attachFileManagement.jsp
@@ -0,0 +1,188 @@
+<!-- 분할 정복으로 첨부파일 관리의 복잡도 관리. 유지보수성 향상 -->
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+   pageEncoding="UTF-8"%>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core"%>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt"%>
+<style>
+#uploadResult {   width: 100%; background-color: gray}
+#uploadResult ul{ display:flex; flex-flow: row; justify-content: center; align-items: center;}
+#uploadResult ul li {list-style:none; padding: 10px; align-content: center; text-align: center;}
+#uploadResult ul li img{ width: 60px;}
+#uploadResult ul li span{color: white;}
+.bigWrapper { position: absolute; display: none; justify-content: center;
+         align-items: center; top: 0%; width: 100%; height: 100%; background-color: gray;
+         z-index: 100; background:rgba(255,255,255,0.5);}
+.bigNested { position: relative; display:flex; justify-content: center; align-items:center;}
+.bigNested img {width: 600px;}
+.bigNested video {width: 600px;}
+</style>
+<!--첨부파일 목록 출력 관련 부분 -->
+<div class="container-fluid">
+   <div class="card shadow mb-4">
+      <div class="card-body">
+         <div class="card-header">첨부파일</div>
+         
+         <div class="card-body" id ="uploadDiv">
+            <input id="inFiles" type="file" name="uploadFile" multiple>
+       		<button id="btnUpload">파일올리기</button>
+         </div>
+         
+         <div class="card-body" id ="uploadResult">
+            <ul></ul>
+         </div>
+         
+         <div class="bigWrapper">
+            <div class="bigNested">
+            </div>
+         </div>
+      </div>
+   </div>
+</div>
+<!-- End of 첨부파일 목록-->
+<script type="text/javascript" src="\resources\js\util\utf8.js"> </script>
+<script type="text/javascript">
+$(document).ready(function(){
+	//업로드 파일에 대한 확장자 제한하는 정규식
+	var uploadConstraintByExt = new RegExp("(.*?)\.(exe|sh|zip|alz)$");
+	//업로드 파일에 대한 최대 크기 제한
+	var uploadMaxSize = 1036870912; /*1GB*/
+	//화면이 맨 처음 로드시 들어 있는 깨끗한 상태 기억
+	var initClearStatus = $("#uploadDiv").clone();
+	
+	$("#btnUpload").on("click", function(e){
+		var formData = new FormData();
+		var files = $("#inFiles")[0].files;
+		
+		for(var i = 0; i < files.length; i++){
+			if(! checkFileConstraints(files[i].name, files[i].size))
+				return false;
+			formData.append("uploadFile", files[i]);
+		}		
+		$.ajax({
+			url : '/uploadFiles/upload',
+			processData : false,
+			contentType : false,
+			data : formData,
+			type : 'post',
+			success : function (result){
+				showUploadedFile(result);				
+				$("#uploadDiv").html(initClearStatus.html());
+			}
+			
+		});
+	});
+	
+	// IE11 까지 고려하여 보여준 이후에 클릭하면 사라지게 한다.
+	$(".bigWrapper").on("click", function () {
+		$(".bigNested").animate({width:'0%', height:'0%'}, 1000);
+		setTimeout( function() {
+			$(".bigWrapper").hide();	
+		}, 1000);
+	});
+	
+
+	
+	
+	//업로드 파일에 대한 제약 사항을 미리 검사해 줍니다.
+	function checkFileConstraints(fileName, fileSize){
+		//크기 검사
+		if(fileSize > uploadMaxSize ) {
+			return false;
+		}
+		//종류 검사
+		if (uploadConstraintByExt.test(fileName)){
+			return false;		
+		}
+		return true;
+	}
+	
+	
+	$("#uploadResult").on("click", "a", function () {
+		var attachVO = $(this).closest("li").data("attach_info");
+		attachVO = JSON.parse(decodeURL(attachVO));
+		
+		$(".bigWrapper").css("display", "flex").show();
+		if(attachVO.multimediaType === "audio"){
+			$(".bigNested").html("<audio src='/uploadFiles/display?fileName=" + encodeURI(attachVO.originalFileCallPath) + "' autoplay>")
+				.animate({width:'100%', height:'100%'}, 1000);
+		}else if (attachVO.multimediaType === "image" ) {			
+			$(".bigNested").html("<img src='/uploadFiles/display?fileName=" + encodeURI(attachVO.originalFileCallPath) + "'>")
+					.animate({width:'100%', height:'100%'}, 1000);
+		}else if (attachVO.multimediaType === "video") {
+			$(".bigNested").html("<video src='/uploadFiles/display?fileName=" + encodeURI(attachVO.originalFileCallPath) + "' autoplay>")
+			.animate({width:'100%', height:'100%'}, 1000);
+		}
+
+	});
+	
+	//첨부 취소하기
+	$("#uploadResult").on("click", "span", function () {
+		var targetLi = $(this).closest("li");
+		var attachVO = targetLi.data("attach_info");
+		attachVO = JSON.parse(decodeURL(attachVO));
+		$.ajax({
+			url : '/uploadFiles/deleteFile',
+			//ajax호출을 json형식으로 할것임
+			data :attachVO,
+			//data를 주니 post형식으로 사용
+			type : 'post',
+			dataType : 'text',
+			success : function (result){
+				targetLi.remove();
+			}
+		});	
+	});
+	
+});	
+
+function showUploadedFile(result){
+	var liTags = "";
+	$(result).each(function(i, attachVOInJson) {
+		//객체로 바꿀것이다.
+		var attachVO = JSON.parse(decodeURL(attachVOInJson));
+		if(attachVO.multimediaType === "others"){
+			liTags += "<li data-attach_info=" + attachVOInJson + "><a href='/uploadFiles/download?fileName=" 
+			+ encodeURIComponent(attachVO.originalFileCallPath) + "'><img src='/resources/img/attachFileIcon.png'>" 
+			+ attachVO.pureFileName + "</a> <span>X<span/></li>";
+		} else {			
+			if (attachVO.multimediaType === "audio") {
+				liTags += "<li data-attach_info=" + attachVOInJson + ">"
+						+"<a>"
+						+"<img src='/resources/img/audioThumbnail.png'>" 
+						+ attachVO.pureFileName + "</a>"
+						+ "<span>X</span>"
+						+ "</li>"; 		
+			}else if (attachVO.multimediaType === "image" || attachVO.multimediaType === "video" ) {
+				liTags += "<li data-attach_info=" + attachVOInJson + ">"
+						+ "<a>"
+						+ "<img src='/uploadFiles/display?fileName=" 
+						+ encodeURIComponent(attachVO.fileCallPath) + "'>" + attachVO.pureFileName + "</a>"
+						+ "<span>X</span>"
+						+ "</li>"; 		
+				}
+		}
+	});		
+ $("#uploadResult ul").append(liTags);
+}
+/**
+ * 첨부 파일 기능은 여러 화면에서 재사용될 가능성이 높다.
+ * 이를 각 화면에서 중복 개발하기 보다는 이곳에서 통합적으로 서비스 할 수 있도록 모듈화시키기.
+ */
+function addAttachInfo(frmContainer, varName) {
+	var inputAttaches = "";
+	$("#uploadResult ul li").each(function(i, attachLi){
+		var jobObj = $(attachLi);
+		
+		var attachVO = jobObj.data("attach_info");
+		//json객체로 만들기
+		attachVO = JSON.parse(decodeURL(attachVO));
+		inputAttaches += "<input type='hidden' name='" + varName + "[" + i + "].uuid' value=" + attachVO.uuid + ">";
+		inputAttaches += "<input type='hidden' name='" + varName + "[" + i + "].savedFolderPath' value=" + attachVO.saved_folder_path + ">";
+		inputAttaches += "<input type='hidden' name='" + varName + "[" + i + "].pureFileName' value=" + attachVO.pureFileName + ">";
+		inputAttaches += "<input type='hidden' name='" + varName + "[" + i + "].multimediaType' value=" + attachVO.multimediaType + ">";
+		
+	});
+	
+	frmContainer.append(inputAttaches);
+}
+</script>
